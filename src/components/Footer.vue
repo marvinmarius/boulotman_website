99981cc56192fdcb214f2ec4596fbd04ed0b4525
Marius <51155542+marvinmarius@users.noreply.github.com>
2020-08-04 13:56:27 +0100

    final update

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,6 @@
 <template>
 
-   <v-container fluid class="brown justify-sm-center justify-space-between " >
+   <v-container fluid class="black  justify-sm-center justify-space-between " >
           <v-row 
               
               class="flex justify-space-between ma-n3 "
@@ -11,8 +11,7 @@
           <router-link :to="`/${$i18n.locale}`" @click.native="scrollToTop">
             <v-img
           alt="boulotman Logo"
-          
-          
+
           src="../assets/boulotmanlogo.png" 
           transition="scale-transition"
           width="150"
@@ -24,12 +23,12 @@
         <v-col cols="12" sm="3" md="3" lg="3">
           
        
-        <v-card-text class="justify-center"><h2 class="text-decoration-underline">Socials</h2></v-card-text>
+        <v-card-text class="justify-center text-decoration-underline xxx">Socials</v-card-text>
         <v-list-item-group class="justify-center grp">
-        <p class="v-list-item--link"><v-icon  size="24px" left>mdi-facebook</v-icon>
+        <p class="v-list-item--link"><v-icon color="yellow"  size="24px" left>mdi-facebook</v-icon>
         <a   href="https://www.facebook.com/boulotman">Facebook</a>  </p>
-        <p class="v-list-item--link"><v-icon size="24px" left>mdi-instagram</v-icon>Instagram</p>
-        <p class="v-list-item--link"><v-icon size="24px" left>mdi-twitter</v-icon>Twitter</p>
+        <p class="v-list-item--link"><v-icon color="yellow"  size="24px" left>mdi-instagram</v-icon>Instagram</p>
+        <p class="v-list-item--link"><v-icon color="yellow" size="24px" left>mdi-twitter</v-icon>Twitter</p>
         </v-list-item-group>
           
 
@@ -38,28 +37,28 @@
         <v-col cols="12" sm="3" md="3" lg="3" >
           
               
-         <v-card-text class="justify-center"><h2 class="text-decoration-underline">{{$t('Useful Links')}}</h2></v-card-text>
+         <v-card-text class="justify-center"><p class="text-decoration-underline xxx">{{$t('Useful Links')}}</p></v-card-text>
     <v-list-item-group class="justify-center flex-column grp">
             <p class="v-list-item--link">
-              <v-icon size="24px" left>mdi-book-open-page-variant</v-icon>
+              <v-icon color="yellow" size="24px" left>mdi-book-open-page-variant</v-icon>
               <router-link class="text--white" @click.native="scrollToTop" :to="`/${$i18n.locale}/about`">
               {{$t('About Us')}}
               </router-link>
               </p>
           <p class="v-list-item--link">
-            <v-icon size="24px" left>mdi-home-circle</v-icon>
+            <v-icon color="yellow" size="24px" left>mdi-home-circle</v-icon>
             <router-link class="text--white" @click.native="scrollToTop" :to="`/${$i18n.locale}`">{{$t('nav.Home')}}
             </router-link>
           </p>
           <p class="v-list-item--link">
-            <v-icon size="24px" left>mdi-shield-lock-outline</v-icon>
+            <v-icon color="yellow" size="24px" left>mdi-shield-lock-outline</v-icon>
             <router-link class="text--white" @click.native="scrollToTop" :to="`/${$i18n.locale}/privacy-policy`" >
             {{$t('Privacy Policy')}}</router-link>
             </p>
             
            
            <p class="v-list-item--link">
-             <v-icon size="24px" left>mdi-book-open</v-icon>
+             <v-icon color="yellow" size="24px" left>mdi-book-open</v-icon>
              <router-link class="text--white" @click.native="scrollToTop" :to="`/${$i18n.locale}/terms-and-conditions`" >
             {{$t('Terms & Conditions')}}
              </router-link>
@@ -76,23 +75,23 @@
         </v-col>
         <v-col cols="12" sm="3" md="3" lg="3" class="flex " >
          
-       <v-card-text class="justify-center"><h2 class="text-decoration-underline">{{ $t('Our Contacts')}}</h2></v-card-text>
+       <v-card-text class="justify-center"><p class="text-decoration-underline xxx">{{ $t('Our Contacts')}}</p></v-card-text>
      <v-list-item-group class="justify-center grp">
-          <p class="v-list-item--link font-smaller"><v-icon size="24px" left>mdi-email</v-icon>boulotman@gmail.com</p>
-         <p class="v-list-item--link font-smaller"><v-icon size="24px" left>mdi-phone</v-icon>(+237)698884618</p>
-          <p class="v-list-item--link "><v-icon size="24px" left>mdi-phone-classic</v-icon>fix: +237 233-478-795</p>
+          <p class="v-list-item--link font-smaller"><v-icon color="yellow" size="24px" left>mdi-email</v-icon>boulotman@gmail.com</p>
+         <p class="v-list-item--link font-smaller"><v-icon color="yellow" size="24px" left>mdi-phone</v-icon>(+237)698884618</p>
+          <p class="v-list-item--link "><v-icon color="yellow" size="24px" left>mdi-phone-classic</v-icon>fix: +237 233-478-795</p>
      </v-list-item-group>
       
          
         </v-col>
     </v-row >
 
-      <v-divider  ></v-divider>
+      <v-divider class="white" ></v-divider>
      
           <v-col cols="12 ">
           
-       <v-card-text class="text-center brown ma-n2"><h2 class="text-decoration-underline">{{ new Date().getFullYear() }} — 
-         <strong>{{ $t('All Rights Reserved')}} </strong></h2></v-card-text>
+       <v-card-text class="text-center black mb-n5 xxxx"><p class="text-decoration-underline"><strong>{{ new Date().getFullYear() }} — </strong>
+         <strong>{{ $t('All Rights Reserved')}} </strong></p></v-card-text>
          
         
    
@@ -117,18 +116,31 @@ export default {
 <style scoped>
 .v-list-item--link a{
  text-decoration: none;
-  color:black;
+  color:wheat;
   
 }
 .grp p {
    
- color:black;
+ color:wheat;
 }
+
 
 .v-list-item--link a:hover{
 
   color:blue;
   
 }
+.xxx{
+  color:wheat;
+  
+    font-weight: 300;
+    font-size: 30px
+}
 
+.xxxx{
+ 
+  color:wheat;
+    font-weight: 300;
+    font-size: 20px
+}
 </style>
