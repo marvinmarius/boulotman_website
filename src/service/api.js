82aa790ffd87311a458855081b523c36100ba67a
marvinmarius <mariusmanela@gmail.com>
2020-08-04 14:10:43 +0000

    Update API service URL.

--- src/service/api.js
+++ src/service/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export default axios.create({
   //baseURL: "http://192.168.100.214:8000/api/v1",
   
-  baseURL: "http://164.90.229.137/api/v1",
+  baseURL: "http://164.90.219.137/api/v1",
   headers: {
    Accept : 'application/json',
     "Content-Type": 'application/json'
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,4 @@
 module.exports = {
-  mode: 'production',
   "transpileDependencies": [
     "vuetify",
     "vue-tel-input-vuetify"
